8e86fb9a2dd28d56f2cc83aaa30e05df41c65516
ayanaibraeva <ayanafront@gmail.com>
2022-11-23 20:59:25 -0800

    Hello World

---
+++ js/script.js
@@ -0,0 +1,257 @@
+// var temp = 3;
+
+// function generateWeather(){
+//     if(temp>0){
+//         console.log("Temp>0")
+//     } else if (temp<0){
+//         console.log ("Temp<0")
+//     }else{
+//         console.log("Equal")
+//     }
+// }
+// generateWeather();
+
+// function calc() {
+//     var a = 5;
+// }
+// console.log(a)
+// calc()
+
+// var name = "leo";
+
+// function greet(){
+//     name = "Alex";
+// }
+
+// greet()
+// console.log(name)
+
+
+// var carKG = "BMWKz";
+
+// function str() {
+//     var carKg = "BMWKG";
+//     console.log(carKg)
+// }
+// str()
+
+
+// var name = "Ajar"
+// function greet(){
+//     var name ="Tilek"
+//     console.log(name)
+// }
+// greet()
+// console.log(name)
+
+// function calc(a,b){
+//     console.log(a+b)
+// }
+// calc(10,2)
+
+
+// function greetNewGamer(userName){
+//     console.log(`Welcome${userName}, lets satrt our game!`)
+// }
+// function continueGame(){
+//     console.log("Welcome Tilek, oue touwn under attack")
+// }
+
+// var askGamer = prompt("New game");
+
+// if(askGamer === "new"){
+//     greetNewGamer(prompt("Whats your name?"))
+// } else if ( askGamer === "continue"){
+//     continueGame(prompt("Whats your name our qween?"))
+// }
+
+// var temp = 15;
+
+//  function generationWeather(temp){
+//     if(temp>0){
+//         cosole/log("Temp>0")
+//     } else if (temp<0){
+//         console.log ("Temp<0")
+//     }else{
+//         console.log("Equal")
+//     }
+// }
+
+// var temp2 = -15;
+
+// generationWeather(temp2)
+
+// var alisher = 0;
+
+// generationWeather(alisher)
+
+// function user(userGame){
+//     console.log( `Welcome our gamer${userGame}!`)
+// }
+// user(prompt("Whats your name?"))
+
+// function calc(a,b){
+//     console.log(a+b)
+// }
+// calc(10,2)
+
+//                                                         Lesson 8
+
+
+// function calc(a, b, c){
+//     return(a + b + c)
+// }
+
+// var res = calc(1, 2, 3,);
+
+// console.log(res)
+
+
+// function retVar(){
+//     var temp = 50;
+//     return temp;
+// }
+
+// var arr = [retVar()]
+// console.log(arr)
+
+
+// function num1(a, b, c){
+//     var res = a + b;
+//     var res1 = res/ c;
+//     return res1;
+// }
+
+// function calc(x, g){
+//     var res = x + g - num1(10, 5, 8);
+//      return res
+// }
+// var res= calc(150,70)
+// console.log(res)
+
+
+// var calc = function (){
+//     var a = 5;
+//     console.log(a)
+// }
+
+// calc()
+
+// function generateArray(array){
+//     for( var i = 0; i < 20 ; i++){
+//         array[i] = i;
+//     }
+
+//     console.log(array)
+// }
+
+// var arr = [];
+// generateArray(arr)
+
+// var arr1 = [];
+// generateArray(arr1)
+
+
+// function greet(userName) {
+//     console.log(`Hi ${userName}`)
+// }
+ 
+// var name = "ALex"
+// greet(name)
+
+// var name2 = "Alid"
+// greet(name2)
+
+//                                        Запись с другой группы 
+
+// function calc(a , b , c){
+//     return ( a + b + c );
+// }
+
+// var res = calc( 12, 3, 1)
+// console.log(res)
+
+// function retVar(){
+//     var temp = 50;
+//     return temp;
+// }
+
+// var arr = [retVar()]
+// console.log(arr)
+
+// function tem(val){
+//     if(val > 0){
+//         return ("Температура больше нуля")
+//     }else if(val < 0){
+//         return ("Температура меньше нуля")
+//     }else{
+//         return ("Температура равна нулю")
+//     }
+// }
+// console.log(tem(5))
+// console.log(tem(-2))
+
+// var greet = function(){
+//     var a = 5;
+//     console.log(a)
+// }
+// greet()
+
+// function generateArray(array){
+//     for( var i = 0; i < 20; i++){
+//         array.push(i)
+//     }
+//     console.log(array)
+// }
+
+// var arr = [];
+// generateArray(arr);
+
+// var arr2 = [];
+// generateArray(arr2)
+
+// var generateArray = function(array){
+//     for(var i = 0; i < 20; i++){
+//         array[i] = i;
+//     }
+//     console.log(array)
+// }
+
+// var arr = [];
+// generateArray(arr)
+
+// const obj = {
+//     generateArray: function(array){
+//         for(var i = 0; i < 20; i++){
+//             array[i] = i;
+//         }
+//         console.log(array)
+//     }
+// }
+// var arr = [];
+// obj.generateArray(arr);
+
+
+// generateArray = (array) => {
+//     for( var i = 0; i < 20; i++){
+//         array[i] = i;
+//     }
+//     console.log(array)
+// }
+// var arr = [];
+// generateArray(arr);
+
+
+
+// generateArray = array =>{
+//         for( var i = 0; i < 20; i++){
+//             array.push(Math.floor(Math.random()*(100-0)+0))
+//         }
+//         console.log(array)
+//     }
+    
+//     const random = [];
+//     generateArray(random);
+    
+  
+
